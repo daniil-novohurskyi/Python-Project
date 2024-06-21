e189217f547039027d59a885bec437d8f7b46263
s28228 <s28228@pjwstk.edu.pl>
2024-06-22 00:19:29 +0200

    Presentation of data in the form of a tabulated table for the possibility of evaluating the data array directly

--- file_handler/file_handler.py
+++ file_handler/file_handler.py
@@ -1,41 +1,44 @@
 import os
 import csv
 import pandas as pd
+from tabulate import tabulate
 
 
 def walk_dir_read(start_dir):
-    # List to hold all DataFrames
+    # maximum output sizes
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.max_rows', None)
+
     dataframes = []
     first_frame = True
     column_names = None
-    skip_line = None
-    # Walk through the directory
+    var = None
+
+    # passing through the directory
     for dirpath, dirnames, filenames in os.walk(start_dir):
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
             if file_path.endswith(".csv"):
-                # Read the CSV file
                 if first_frame:
                     with open(file_path, 'r', newline='', encoding='utf-8') as file:
-                        # Create a CSV reader object
                         reader = csv.reader(file)
-                        # Read the first row (header)
                         first_row = next(reader)
-                    # Extract column names from the first row
                         column_names = first_row
                         first_frame = False
                 with open(file_path, 'r', newline='', encoding='utf-8') as file:
-                    # Create a CSV reader object
                     reader = csv.reader(file)
-                    # Read the first row (header)
                     first_row = next(reader)
                     skip_line = 1 if first_row == column_names else 0
                 df = pd.read_csv(file_path, skiprows=skip_line)
                 df.columns = column_names
-                # Append the DataFrame to the list
                 dataframes.append(df)
+                # output of the received data from the file
+                # print(f"Data from file {filename}:")
+                # print(tabulate(df, headers='keys', tablefmt='psql'))
 
-    # Concatenate all DataFrames column-wise with unique suffixes
     combined_df = pd.concat(dataframes, ignore_index=True)
+    # output of the received data collection
+    # print(tabulate(combined_df, headers='keys', tablefmt='psql'))
 
     return combined_df
