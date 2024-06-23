2c1fe4190c1b5c015e95fd52512f88665b1a292a
s28228 <s28228@pjwstk.edu.pl>
2024-06-23 14:43:23 +0200

    Structuring the code

---
+++ handler/data_handler.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from tabulate import tabulate
+
+# maximum console output sizes settings
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', None)
+
+
+def process_data(combined_df):
+    # 1. sorting by time
+    date_column = 'Data wpisu na lpt'
+    combined_df[date_column] = combined_df[date_column].apply(convert_to_date)
+    combined_df = combined_df.sort_values(by=date_column)
+    combined_df[date_column] = pd.to_datetime(combined_df[date_column])
+
+    # 2. bringing provinces to a single appearance
+    combined_df['Województwo'] = combined_df['Województwo'].str.lower().str.strip()
+
+    # 3. drop duplicates and NaN's
+    combined_df = combined_df.drop_duplicates()
+    combined_df = combined_df.dropna()
+
+    return combined_df
+
+
+def convert_to_date(date_str):
+    for fmt in ('%Y-%m-%d %H:%M:%S', '%Y-%m-%d'):
+        try:
+            return pd.to_datetime(date_str, format=fmt).strftime('%Y-%m-%d')
+        except (ValueError, TypeError):
+            continue
+    return None
+
+
+def print_data_summary(df):
+    print(tabulate(df, headers='keys', tablefmt='psql'))
+    print(f"The number of records: {df.shape[0]}")
--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from tkinter import Tk
 
 from DataAnalysis import DataAnalysis
 from DataAnalysisGUI import DataAnalysisGUI
-from file_handler.file_handler import walk_dir_read
+from handler.data_handler import process_data, print_data_summary
+from handler.file_handler import walk_dir_read
 
 if __name__ == "__main__":
     start_dir = 'data'
     combined_df = walk_dir_read(start_dir)
-    analysis = DataAnalysis(combined_df)
+    processed_df = process_data(combined_df)
+    # print_data_summary(combined_df)
+    print_data_summary(processed_df)
+
+    analysis = DataAnalysis(processed_df)
 
     root = Tk()
     root.title("Data Analysis GUI")
