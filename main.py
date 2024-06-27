3c6fcb27103a6705b067f9f7d15b1c99a9988324
s28328 <s28328@pjwstk.edu.pl>
2024-06-27 17:14:11 +0200

    Added calling of GeneralDataDiagram

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from models.GeneralDataProcessor import GeneralDataProcessor
 from services.FrameManager import FrameManager
+from view.GeneralDataFrame import GeneralDataFrame
 from view.TableFrame import TableFrame
 from view.MapFrame import MapFrame
 from handlers.DataHandler import process_data, print_data_summary
@@ -29,6 +31,8 @@ if __name__ == "__main__":
     plot_processor = PlotProcessor(processed_df)
     pie_chart_processor = PieChartDataProcessor(processed_df)
     data_processor = MapDataProcessor(processed_df)
+    district_data = GeneralDataProcessor(processed_df, 'Województwo', "Histogram rozkładu według regionu", "Województwo")
+    product_data = GeneralDataProcessor(processed_df,'Rodzaj produktu rolnego, środka spożywczego lub napoju spirytusowego wpisanego na lpt',"Histogram rozkładu według rodzaju produktu", "Rodzaj produktu")
 
     # Create the main application with a frame manager
     app = FrameManager()
@@ -36,13 +40,14 @@ if __name__ == "__main__":
     app.attributes('-fullscreen', False)
 
     # Add frames with different visualizations to the application
+    app.add_frame(TableFrame, processed_df)  # Frame for table
+    app.add_frame(GeneralDataFrame, district_data, product_data)
     app.add_frame(PlotFrame, plot_processor)  # Frame for tables
     app.add_frame(MapFrame, data_processor)  # Frame for maps
     app.add_frame(PieChartFrame, pie_chart_processor)  # Frame for pie chart
-    app.add_frame(TableFrame, processed_df)  # Frame for table
 
     # Show the initial frame when the application starts
-    app.show_frame(PlotFrame)
+    app.show_frame(TableFrame)
 
     # Run the main loop of the application
     app.mainloop()
